cbce43340643e8be20d13cbd69b66d3e45f10fbd
JuanjoUnizar756640 <756640@unizar.es>
2025-04-21 19:45:24 +0200

    Inicializa backend Spring Boot para TFG

---
+++ src/main/java/com/mercadona/mercadona_caducados/MercadonaCaducadosApplication.java
@@ -0,0 +1,13 @@
+package com.mercadona.mercadona_caducados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadonaCaducadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadonaCaducadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadona/mercadona_caducados/MercadonaCaducadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.mercadona_caducados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadonaCaducadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
